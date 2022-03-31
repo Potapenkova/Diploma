0ba8a845585c317ad2f02580acf64e8e09e22023
Potapenkova <potapenkowa2016@yandex.ru>
2022-03-31 08:53:58 +0300

    blazor_Orange

--- BlazorAdminPanel/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/City.razor.g.cs
+++ BlazorAdminPanel/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/City.razor.g.cs
@@ -13,77 +13,77 @@ namespace BlazorAdminPanel.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\User\Desktop\Diplom\Аптека\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
+#line 1 "C:\Users\User\Desktop\Diplom\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\User\Desktop\Diplom\Аптека\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
+#line 2 "C:\Users\User\Desktop\Diplom\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\User\Desktop\Diplom\Аптека\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
+#line 3 "C:\Users\User\Desktop\Diplom\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\User\Desktop\Diplom\Аптека\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
+#line 4 "C:\Users\User\Desktop\Diplom\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\User\Desktop\Diplom\Аптека\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
+#line 5 "C:\Users\User\Desktop\Diplom\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\User\Desktop\Diplom\Аптека\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
+#line 6 "C:\Users\User\Desktop\Diplom\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\User\Desktop\Diplom\Аптека\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
+#line 7 "C:\Users\User\Desktop\Diplom\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\User\Desktop\Diplom\Аптека\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
+#line 8 "C:\Users\User\Desktop\Diplom\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
 using BlazorAdminPanel;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\User\Desktop\Diplom\Аптека\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
+#line 9 "C:\Users\User\Desktop\Diplom\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
 using BlazorAdminPanel.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\User\Desktop\Diplom\Аптека\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
+#line 10 "C:\Users\User\Desktop\Diplom\BlazorAdminPanel\BlazorAdminPanel\_Imports.razor"
 using BlazorAdminPanel.Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\User\Desktop\Diplom\Аптека\BlazorAdminPanel\BlazorAdminPanel\Pages\City.razor"
+#line 4 "C:\Users\User\Desktop\Diplom\BlazorAdminPanel\BlazorAdminPanel\Pages\City.razor"
 using BlazorAdminPanel.Shared.MyComponent;
 
 #line default
@@ -98,7 +98,7 @@ using BlazorAdminPanel.Shared.MyComponent;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 62 "C:\Users\User\Desktop\Diplom\Аптека\BlazorAdminPanel\BlazorAdminPanel\Pages\City.razor"
+#line 62 "C:\Users\User\Desktop\Diplom\BlazorAdminPanel\BlazorAdminPanel\Pages\City.razor"
        
 
 
